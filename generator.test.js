056c90fd218013934b20985f35bc5d4b4d562582
luoyangping <luoyangping@supermokey.com.cn>
2020-03-05 22:59:18 +0800

    更新

--- generator.test.js
+++ generator.test.js
@@ -1,4 +1,4 @@
-import { select, sagaRunnerCreator, someSaga } from './generator'
+import { select, sagaRunnerCreator } from './generator'
 
 test('should create "select" effect object', () => {
     function selector() {
@@ -10,3 +10,28 @@ test('should create "select" effect object', () => {
         args: ['name']
     })
 })
+
+test('should create "select" effect object', done => {
+    function getState() {
+        return {userInfo: {id: '123', name: 'Deep'}}
+    }
+    const getUserInfo = (state) => state.userInfo
+    const getUserProperty = (state, key) => state.userInfo[key]
+
+    function* someSaga() {
+        try {
+            const userInfo = yield select(getUserInfo)
+            expect(userInfo).toEqual({
+                id: '123',
+                name: 'Deep'
+            });
+            const userProperty = yield select(getUserProperty, 'name')
+            expect(userProperty).toBe('Deep');
+            done();
+        } catch (error) {
+            done(error);
+        }
+    }
+    const sagaRunner = sagaRunnerCreator({getState})
+    sagaRunner.run(someSaga)
+});
